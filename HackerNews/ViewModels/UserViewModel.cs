9c119e96aa4303ad444c0487f3b2e7840615a0ba
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-17 12:17:24 +0100

    Create UserViewModel.cs

---
+++ HackerNews/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HackerNews.ViewModels;
+
+public partial class UserViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _id;
+    [ObservableProperty] private int _created;
+    [ObservableProperty] private int _karma;
+    [ObservableProperty] private string _about;
+    [ObservableProperty] private List<int> _submitted;
+}
