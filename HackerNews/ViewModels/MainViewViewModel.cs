968c2332165f0fe85d2268e7373e10bf89631490
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-17 12:24:12 +0100

    Update MainViewViewModel.cs

--- HackerNews/ViewModels/MainViewViewModel.cs
+++ HackerNews/ViewModels/MainViewViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
@@ -27,31 +28,69 @@ public partial class MainViewViewModel : ViewModelBase
     {
         _items.Clear();
 
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
         var client = new HttpClient();
-        var urlTopStories = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
-        var response = await client.GetAsync(urlTopStories);
-        var json = await response.Content.ReadAsStringAsync();
-        _topStoriesIds = JsonSerializer.Deserialize<List<int>>(json);
+
+        var topStoriesJson = await GetTopStoriesJson(client);
+        _topStoriesIds = JsonSerializer.Deserialize<List<int>>(topStoriesJson, options);
 
         var storiesIds = _topStoriesIds.Take(20);
         var index = 1;
 
         foreach (var id in storiesIds) 
         {
-            var urlItemId = $"https://hacker-news.firebaseio.com/v0/item/{id}.json?print=pretty";
-            var itemResponse = await client.GetAsync(urlItemId);
-            var itemJson = await itemResponse.Content.ReadAsStringAsync();
+            var itemJson = await GetItemJson(id, client);
+            var item = default(ItemViewModel);
 
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
             await Task.Run(() =>
             {
-                var item = JsonSerializer.Deserialize<ItemViewModel>(itemJson, options);
-                item.Index = index++;
-                _items.Add(item);
+                item = JsonSerializer.Deserialize<ItemViewModel>(itemJson, options);
+                if (item is { })
+                {
+                    item.Index = index++;
+                    _items.Add(item);
+                }
             });
+
+            /*
+            if (item?.By is { })
+            {
+                var userJson = await GetItemJson(item.By, client);
+                var user = JsonSerializer.Deserialize<UserViewModel>(userJson, options);
+                if (user is { })
+                {
+                    Debug.WriteLine(userJson);
+                }
+            }
+            //*/
         }
+    }
+
+    private static async Task<string> GetTopStoriesJson(HttpClient client)
+    {
+        var requestUri = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+        var response = await client.GetAsync(requestUri);
+        var json = await response.Content.ReadAsStringAsync();
+        return json;
+    }
+
+    private static async Task<string> GetItemJson(int id, HttpClient client)
+    {
+        var requestUri = $"https://hacker-news.firebaseio.com/v0/item/{id}.json?print=pretty";
+        var response = await client.GetAsync(requestUri);
+        var json = await response.Content.ReadAsStringAsync();
+        return json;
+    }  
+
+    private static async Task<string> GetItemJson(string username, HttpClient client)
+    {
+        var requestUri = $"https://hacker-news.firebaseio.com/v0/user/{username}.json?print=pretty";
+        var response = await client.GetAsync(requestUri);
+        var json = await response.Content.ReadAsStringAsync();
+        return json;
     }
 }
