1b281e1915b01f8764e84f14b093dcaaeb543fd4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-17 22:19:15 +0100

    Update ItemsListBox.cs

--- HackerNews/Controls/ItemsListBox.cs
+++ HackerNews/Controls/ItemsListBox.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Styling;
@@ -49,6 +48,5 @@ public class ItemsListBox : ListBox, IStyleable
             }
         }
     }
-
 }
 
