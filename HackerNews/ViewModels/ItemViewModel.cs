ff508bc3d63d956b0ef22080bb3705d4f6adb3a0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-17 21:36:42 +0100

    Update ItemViewModel.cs

--- HackerNews/ViewModels/ItemViewModel.cs
+++ HackerNews/ViewModels/ItemViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using HackerNews.Model;
 
 namespace HackerNews.ViewModels;
 
 public partial class ItemViewModel : ViewModelBase
 {
+    private Item _item;
     [ObservableProperty] private int _index;
     [ObservableProperty] private int _id;
     [ObservableProperty] private bool _deleted;
     [ObservableProperty] private string _type;
-    [ObservableProperty] private string _by;
+    [ObservableProperty] private UserViewModel? _by;
     [ObservableProperty] private int _time;
     [ObservableProperty] private string _text;
     [ObservableProperty] private bool _dead;
@@ -21,4 +24,17 @@ public partial class ItemViewModel : ViewModelBase
     [ObservableProperty] private string _title;
     [ObservableProperty] private List<int> _parts;
     [ObservableProperty] private int? _descendants;
+
+    public ItemViewModel(Item item)
+    {
+        _item = item;
+    }
+
+    public void Update()
+    {
+        Index = _item.Index;
+        Score = _item.Score;
+        Title = _item.Title;
+        // TODO:
+    }
 }
