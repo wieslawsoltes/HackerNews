9ed045f5b2e618df8a66f73d454c109a37fc38db
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-17 21:36:46 +0100

    Update UserViewModel.cs

--- HackerNews/ViewModels/UserViewModel.cs
+++ HackerNews/ViewModels/UserViewModel.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections.Generic;
 using CommunityToolkit.Mvvm.ComponentModel;
+using HackerNews.Model;
 
 namespace HackerNews.ViewModels;
 
 public partial class UserViewModel : ViewModelBase
 {
+    private User _user;
     [ObservableProperty] private string _id;
     [ObservableProperty] private int _created;
     [ObservableProperty] private int _karma;
     [ObservableProperty] private string _about;
     [ObservableProperty] private List<int> _submitted;
+
+    public UserViewModel(User user)
+    {
+        _user = user;
+    }
+
+    public void Update()
+    {
+        Id = _user.Id;
+        Created = _user.Created;
+        Karma = _user.Karma;
+        About = _user.About;
+        // TODO:
+    }
 }
