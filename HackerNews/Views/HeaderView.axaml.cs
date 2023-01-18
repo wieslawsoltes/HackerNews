4af5bf72ca00076e98fa199dcb8c601e67208477
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-18 21:51:09 +0100

    Added HeaderView

---
+++ HackerNews/Views/HeaderView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HackerNews.Views;
+
+public partial class HeaderView : UserControl
+{
+    public HeaderView()
+    {
+        InitializeComponent();
+    }
+}
+
