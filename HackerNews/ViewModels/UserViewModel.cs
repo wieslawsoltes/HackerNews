e4beec93b5fa59441c949818a6375a99ebafddb2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-18 10:54:47 +0100

    Update UserViewModel.cs

--- HackerNews/ViewModels/UserViewModel.cs
+++ HackerNews/ViewModels/UserViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using HackerNews.Model;
 using HackerNews.Services;
@@ -7,25 +8,41 @@ namespace HackerNews.ViewModels;
 
 public partial class UserViewModel : ViewModelBase
 {
-    private readonly HackerNewsApiV0 _api;
+    private HackerNewsApiV0? _api;
     private User? _user;
-    [ObservableProperty] private string _id;
+    [ObservableProperty] private string? _id;
     [ObservableProperty] private int _created;
     [ObservableProperty] private int _karma;
     [ObservableProperty] private string _about;
     [ObservableProperty] private List<int> _submitted;
 
-    public UserViewModel(HackerNewsApiV0 api, User user)
+    public UserViewModel()
     {
-        _user = user;
+    }
+
+    public UserViewModel(HackerNewsApiV0 api, string id)
+    {
+        _api = api;
+        _id = id;
+    }
+
+    public async Task Load()
+    {
+        if (_api is { } && _id is { })
+        {
+            var json = await _api.GetUserJson(_id);
+            _user = await _api.DeserializeAsync<User>(json);
+        }
     }
 
     public void Update()
     {
-        Id = _user.Id;
-        Created = _user.Created;
-        Karma = _user.Karma;
-        About = _user.About;
-        // TODO:
+        if (_user is { })
+        {
+            Created = _user.Created;
+            Karma = _user.Karma;
+            About = _user.About;
+            // TODO:
+        }
     }
 }
