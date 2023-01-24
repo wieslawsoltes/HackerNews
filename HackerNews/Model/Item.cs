67ec66eb00817d94f8d05790afd117e740ad2790
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-24 22:42:51 +0100

    Update Item.cs

--- HackerNews/Model/Item.cs
+++ HackerNews/Model/Item.cs
@@ -6,17 +6,17 @@ public class Item
 {
     public int Id { get; set; }
     public bool Deleted { get; set; }
-    public string Type { get; set; }
-    public string By { get; set; }
+    public string? Type { get; set; }
+    public string? By { get; set; }
     public int Time { get; set; }
-    public string Text { get; set; }
+    public string? Text { get; set; }
     public bool Dead { get; set; }
     public int? Parent { get; set; }
     public int? Poll { get; set; }
-    public List<int> Kids { get; set; }
-    public string Url { get; set; }
+    public List<int>? Kids { get; set; }
+    public string? Url { get; set; }
     public int Score { get; set; }
-    public string Title { get; set; }
-    public List<int> Parts { get; set; }
+    public string? Title { get; set; }
+    public List<int>? Parts { get; set; }
     public int? Descendants { get; set; }
 }
