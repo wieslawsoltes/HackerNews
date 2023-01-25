a0aea659d98559b923f0c42bb8fd3a6bbaca2a4e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-25 21:38:34 +0100

    Add HackerNews.Android

---
+++ HackerNews.Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia.Android;
+
+namespace HackerNews.Android;
+
+[Activity(Label = "HackerNews", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
---
+++ HackerNews.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Avalonia.Android;
+
+namespace HackerNews.Android;
+
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : AvaloniaSplashActivity<App>
+{
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+    }
+}
