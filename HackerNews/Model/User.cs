ac0bc89a1b3da5c7955e5eb006929048a75f39db
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-25 21:21:27 +0100

    Update User.cs

--- HackerNews/Model/User.cs
+++ HackerNews/Model/User.cs
@@ -4,9 +4,9 @@ namespace HackerNews.Model;
 
 public class User
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public int Created { get; set; }
     public int Karma { get; set; }
-    public string About { get; set; }
-    public List<int> Submitted { get; set; }
+    public string? About { get; set; }
+    public List<int>? Submitted { get; set; }
 }
