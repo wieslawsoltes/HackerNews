200cee11b87433f2544d3af22bb407019b62ef9f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-30 20:19:24 +0100

    Update StringConverter.cs

--- HackerNews/Converters/StringConverter.cs
+++ HackerNews/Converters/StringConverter.cs
@@ -6,77 +6,176 @@ using HtmlAgilityPack;
 
 namespace HackerNews.Converters;
 
-public abstract class Node
+public class Node
 {
-    public List<Node> Nodes { get; }
-
-    protected Node()
-    {
-        Nodes = new List<Node>();
-    }
+    public List<Node> Nodes { get; set; } = new ();
 }
 
 public class TextNode : Node
 {
-    public string Text { get; }
+    public string? Text { get; set; }
+}
 
-    public TextNode(string text)
-    {
-        Text = text;
-    }
+public class ItalicNode : Node
+{
+    public Node? Content { get; set; }
+}
+
+public class QuoteNode : TextNode
+{
+}
+
+public class ParagraphNode : Node
+{
 }
 
 public class AnchorNode : Node
 {
-    public Node Content { get;  }
-
-    public string Href { get;  }
-
-    public AnchorNode(Node content, string href)
-    {
-        Content = content;
-        Href = href;
-    }
+    public string? Href { get;set; }
 }
 
-public class Layout
+public class PreNode : Node
 {
-    public List<Node> Nodes { get; set; }
+}
 
-    public Layout()
-    {
-        Nodes = new List<Node>();
-    }
+public class CodeNode : Node
+{
+    public Node? Content { get; set; }
 }
 
 public static class StringConverter
 {
-    public static string? ParseHtmlString(string? text)
+    private static void ParseHtml(HtmlNode htmlNode, Node root, int level)
     {
-        if (string.IsNullOrWhiteSpace(text))
+        foreach (var htmlNodeDescendant in htmlNode.Descendants())
         {
-            return text;
-        }
-
-        
-        
-        var doc = new HtmlDocument();
-        doc.LoadHtml(text);
-
-        Console.WriteLine($"[HtmlDocument]");
-        foreach (var htmlNode in doc.DocumentNode.Descendants())
-        {
-            Console.WriteLine($"  {htmlNode.Name})");
-
-            if (htmlNode.HasChildNodes)
+            //Console.WriteLine($"{new string(' ', level * 2)}[{htmlNodeDescendant.Name}]");
+            var name = htmlNodeDescendant.Name;
+            switch (name)
             {
-                // TODO:
+                case "#text":
+                {
+                    var innerText = HttpUtility.HtmlDecode(htmlNodeDescendant.InnerText);
+                    if (string.IsNullOrEmpty(innerText))
+                    {
+                        break;
+                    }
+
+                    if (innerText.StartsWith('>'))
+                    {
+                        var quote = new QuoteNode
+                        {
+                            Text = innerText.TrimStart('>').TrimStart()
+                        };
+                        root.Nodes.Add(quote);
+                    }
+                    else
+                    {
+                        var text = new TextNode
+                        {
+                            Text = innerText
+                        };
+                        root.Nodes.Add(text);
+                    }
+
+                    break;
+                }
+                case "p":
+                {
+                    var p = new ParagraphNode();
+                    if (htmlNodeDescendant.HasChildNodes)
+                    {
+                        ParseHtml(htmlNodeDescendant, p, level + 1);
+                    }
+                    root.Nodes.Add(p);
+                    break;
+                }
+                case "a":
+                {
+                    var href = HttpUtility.HtmlDecode(htmlNodeDescendant.GetAttributeValue("href", null));
+                    var a = new AnchorNode
+                    {
+                        Href = href
+                    };
+                    if (htmlNodeDescendant.HasChildNodes)
+                    {
+                        ParseHtml(htmlNodeDescendant, a, level + 1);
+                    }
+                    root.Nodes.Add(a);
+                    break;
+                }
+                case "i":
+                {
+                    var i = new ItalicNode();
+
+                    if (htmlNodeDescendant.HasChildNodes)
+                    {
+                        ParseHtml(htmlNodeDescendant, i, level + 1);
+                    }
+
+                    root.Nodes.Add(i);
+                    break;
+                }
+                case "pre":
+                {
+                    var pre = new PreNode();
+                    if (htmlNodeDescendant.HasChildNodes)
+                    {
+                        ParseHtml(htmlNodeDescendant, pre, level + 1);
+                    }
+                    root.Nodes.Add(pre);
+                    break;
+                }
+                case "code":
+                {
+                    var code = new CodeNode();
+                    if (htmlNodeDescendant.HasChildNodes)
+                    {
+                        ParseHtml(htmlNodeDescendant, code, level + 1);
+                    }
+                    root.Nodes.Add(code);
+                    break;
+                }
+                default:
+                {
+                    //Console.WriteLine($"{new string(' ', level * 2)}Unknown node name: {name}.");
+                    break;
+                }
             }
         }
+    }
 
+    private static void PrintNode(Node node, int level)
+    {
+        Console.WriteLine($"{new string(' ', level * 2)}{node.GetType().Name}");
+
+        foreach (var childNode in node.Nodes)
+        {
+            PrintNode(childNode, level + 1);
+        }
+    }
+
+    public static string? ParseHtmlString(string? html)
+    {
+        if (string.IsNullOrWhiteSpace(html))
+        {
+            return html;
+        }
+
+
+        var root = new Node();
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        //Console.WriteLine("");
+        ParseHtml(doc.DocumentNode, root, 0);
+        PrintNode(root, 0);
         
         
-        var decoded = HttpUtility.HtmlDecode(text);
+        
+
+        // TODO: Remove below old code.
+
+        var decoded = HttpUtility.HtmlDecode(html);
 
         // TODO: Parse string to text layout object tree.
 
