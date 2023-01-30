62220af6309b1f1703dcdaa6cda75d0fcce140fb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-30 21:50:19 +0100

    Update AppDelegate.cs

--- HackerNews.iOS/AppDelegate.cs
+++ HackerNews.iOS/AppDelegate.cs
@@ -1,5 +1,9 @@
+using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.iOS;
 using Foundation;
+using HackerNews.Model;
+using Xamarin.Essentials;
 
 namespace HackerNews.iOS;
 
@@ -7,7 +11,32 @@ namespace HackerNews.iOS;
 // User Interface of the application, as well as listening (and optionally responding) to 
 // application events from iOS.
 [Register("AppDelegate")]
-public class AppDelegate : AvaloniaAppDelegate<App>
+public class AppDelegate : AvaloniaAppDelegate<App>, IBrowserService
 {
-        
+    public async Task OpenBrowserAsync(System.Uri uri, bool external = false)
+    {
+        try
+        {
+            await Browser.OpenAsync(
+                uri, 
+                external ? BrowserLaunchMode.External : BrowserLaunchMode.SystemPreferred);
+
+            // TODO: External alternative.
+            // var intent = new Intent (Intent.ActionView, Uri.Parse (uri.ToString()));
+            // StartActivity (intent);
+        }
+        catch (System.Exception)
+        {
+            // ignored
+        }
+    }
+
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .AfterSetup(_ =>
+            {
+                App.BrowserService = this;
+            });
+    }
 }
