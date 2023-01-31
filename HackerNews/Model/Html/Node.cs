54110cc9925edbaa1214723896fa77bab49e92ed
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-31 14:25:41 +0100

    Update Node.cs

--- HackerNews/Model/Html/Node.cs
+++ HackerNews/Model/Html/Node.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using Avalonia.Metadata;
 
 namespace HackerNews.Model.Html;
 
 public class Node
 {
+    [Content]
     public List<Node> Nodes { get; set; } = new ();
 }
