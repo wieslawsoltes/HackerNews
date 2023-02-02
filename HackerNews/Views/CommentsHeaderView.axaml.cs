1e136c78265a0729cc6d81b60bbdcd57b5d52af0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-02 13:42:36 +0100

    Add CommentsHeaderView

---
+++ HackerNews/Views/CommentsHeaderView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HackerNews.Views;
+
+public partial class CommentsHeaderView : UserControl
+{
+    public CommentsHeaderView()
+    {
+        InitializeComponent();
+    }
+}
+
