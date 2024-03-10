0d6f5c247df7280b086ef975680b7239e53785a2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-03-10 13:05:49 +0100

    Update MainView.axaml.cs

--- HackerNews/Views/MainView.axaml.cs
+++ HackerNews/Views/MainView.axaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using HackerNews.Model;
+using HackerNews.ViewModels;
 
 namespace HackerNews.Views;
 
 public partial class MainView : UserControl
 {
+    private Vector _delta;
+
     public MainView()
     {
         InitializeComponent();
@@ -42,31 +46,163 @@ public partial class MainView : UserControl
 
     private async void OnPullGesture(object? sender, PullGestureEventArgs e)
     {
-        //Console.WriteLine($"PullGesture {e.Id} {e.PullDirection} {e.Delta}");
+        // Console.WriteLine($"PullGesture {e.Id} {e.PullDirection} {e.Delta}");
+
         switch (e.PullDirection)
         {
             case PullDirection.LeftToRight:
+            {
+                _delta = e.Delta;
+
                 // TODO: Show hamburger menu.
                 // SplitView.SetValue(SplitView.IsPaneOpenProperty, true);
                 // e.Handled = true;
-                if (DataContext is ILazyLoadable lazyLoadable)
+
+                /*
+                if (DataContext is MainViewViewModel mainViewViewModel && mainViewViewModel.Navigation is { } navigation)
                 {
-                    var handled = await lazyLoadable.BackAsync();
-                    e.Handled = handled;
+                    if (navigation.CanGoBack)
+                    {
+                        if (DataContext is ILazyLoadable lazyLoadable)
+                        {
+                            var handled = await lazyLoadable.BackAsync();
+                            e.Handled = handled;
+                            return;
+                        }
+                    }
                 }
+                */
+
                 break;
+            }
             case PullDirection.RightToLeft:
+            {
+                _delta = e.Delta;
+
                 // TODO: Hide hamburger menu.
                 // SplitView.SetValue(SplitView.IsPaneOpenProperty, false);
                 // e.Handled = true;
+                
+                /*
+                if (DataContext is MainViewViewModel mainViewViewModel && mainViewViewModel.Navigation is { } navigation)
+                {
+                    if (navigation.CanGoBack)
+                    {
+                        // TODO:
+                    }
+                }
+                */
+
                 break;
+            }
+
+            case PullDirection.TopToBottom:
+                _delta = new Vector();
+                break;
+            case PullDirection.BottomToTop:
+                _delta = new Vector();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
     }
 
-    private void PullGestureEnded(object? sender, PullGestureEndedEventArgs e)
+    private async void PullGestureEnded(object? sender, PullGestureEndedEventArgs e)
     {
-        //Console.WriteLine($"PullGestureEnded {e.Id} {e.PullDirection}");
+        // Console.WriteLine($"PullGestureEnded {e.Id} {e.PullDirection}");
         // TODO: Handle hamburger menu show/hide state.
+
+        switch (e.PullDirection)
+        {
+            case PullDirection.LeftToRight:
+            {
+                if (DataContext is MainViewViewModel mainViewViewModel && mainViewViewModel.Navigation is { } navigation)
+                {
+                    if (_delta.X < 30)
+                    {
+                        return;
+                    }
+
+                    if (navigation.CanGoBack)
+                    {
+                        if (DataContext is ILazyLoadable lazyLoadable)
+                        {
+                            var handled = await lazyLoadable.BackAsync();
+                            e.Handled = handled;
+                            return;
+                        }
+                    }
+
+                    if (!navigation.CanGoBack)
+                    {
+                        if (mainViewViewModel.CurrentFeed is not null && mainViewViewModel.Feeds is not null)
+                        {
+                            var index = mainViewViewModel.Feeds.IndexOf(mainViewViewModel.CurrentFeed);
+                            var totalFeeds = mainViewViewModel.Feeds.Count;
+
+                            var nextIndex = index - 1;
+                            if (nextIndex >= 0)
+                            {
+                                e.Handled = true;
+                                _delta = new Vector();
+                                await mainViewViewModel.OpenFeedAsync(mainViewViewModel.Feeds[nextIndex]);
+                            }
+                            else
+                            {
+                                e.Handled = true;
+                                _delta = new Vector();
+                                await mainViewViewModel.OpenFeedAsync(mainViewViewModel.Feeds[totalFeeds - 1]); 
+                            }
+                        }
+                    }
+                }
+
+                break;
+            }
+            case PullDirection.RightToLeft:
+            {
+                if (DataContext is MainViewViewModel mainViewViewModel && mainViewViewModel.Navigation is { } navigation)
+                {
+                    if (!navigation.CanGoBack)
+                    {
+                        if (_delta.X < 30)
+                        {
+                            return;
+                        }
+
+                        if (mainViewViewModel.CurrentFeed is not null && mainViewViewModel.Feeds is not null)
+                        {
+                            var index = mainViewViewModel.Feeds.IndexOf(mainViewViewModel.CurrentFeed);
+                            var totalFeeds = mainViewViewModel.Feeds.Count;
+
+                            var nextIndex = index + 1;
+                            if (nextIndex < totalFeeds)
+                            {
+                                e.Handled = true;
+                                _delta = new Vector();
+                                await mainViewViewModel.OpenFeedAsync(mainViewViewModel.Feeds[nextIndex]);
+                                return;
+                            }
+                            else
+                            {
+                                e.Handled = true;
+                                _delta = new Vector();
+                                await mainViewViewModel.OpenFeedAsync(mainViewViewModel.Feeds[0]); 
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                break;
+            }
+            case PullDirection.TopToBottom:
+                break;
+            case PullDirection.BottomToTop:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     private async void TopLevelOnBackRequested(object? sender, RoutedEventArgs e)
